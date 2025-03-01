3411f2fc5b0603bda53a4fe13a724e2b604b3797
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-03-02 00:16:00 +0200

    added If Statement

---
+++ IfStatements/IfStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if statement = a basic form of decision making
+
+            Console.WriteLine("Please enter your name: ");
+            String name = Console.ReadLine();
+
+            if (name == "")
+            {
+                Console.WriteLine("You did not enter your name!");
+            }
+            else
+            {
+                Console.WriteLine("Hello " + name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
